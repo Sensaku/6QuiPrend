2ed4a0fdbed7f5aa492143226ca9edc2db8c6074
Sensaku <barbe.arnaud@hotmail.fr>
2021-06-22 19:34:12 +0200

    Ajout des choix de joueur dans Board

--- src/Board.py
+++ src/Board.py
@@ -2,7 +2,27 @@ class Ligne:
     def __init__(self):
         self.cards = []
 
+    def to_string(self):
+        result = ""
+        for case in self.cards:
+            result += case.to_string()
+        return result
+
+
 class Board:
 
     def __init__(self):
-        self.ligne = []
+        self.lignes = []
+        self.plie = []
+        for i in range(4):
+            self.lignes.append(Ligne())
+
+    def init_board(self, cards):
+        for ligne in self.lignes:
+            ligne.cards.append(cards.pop())
+
+    def __repr__(self):
+        result = f"Tableau de jeu\n"
+        for ligne in self.lignes:
+            result += ligne.to_string()
+        return result
